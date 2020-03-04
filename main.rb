c131cb01822979e27064d6b6ff9df68faf827efa
n.shmelyov <shmelev507@gmail.com>
2020-03-04 07:11:46 +0300

    work with links

--- main.rb
+++ main.rb
@@ -19,11 +19,13 @@ i = 2
 
 until links.empty?
 
-  url = if url.include?('?p=')
-          url[0, url.length - 1] + (url[-1].to_i + (2 - 1)).to_s
-        else
-          url + '?p=' + i.to_s
-        end
+  if url.include?('/?p=') && url[-1] != '/'
+    url = url[0, url.length - 1] + (url[-1].to_i + (2 - 1)).to_s
+  elsif !url.include?('/?p=') && url[-1] == '/' 
+    url = url + '/?p=' + i.to_s
+  else
+    break 
+  end
 
   doc = Nokogiri::HTML(Curl.get(url).body_str)
   links = doc.search('div.pro_outer_box a.product-name').map { |link| link['href'] }
@@ -32,21 +34,23 @@ until links.empty?
 end
 
 puts "Page count: #{links_on_pages.length - 1}"
+
 CSV.open("#{csv_name}.csv", 'wb') do |csv|
   puts 'CSV file has been created'
 
   csv << %w[Name Price Image]
 
-  links_on_pages[0, links_on_pages.length - 1].each_with_index do |links, item_index|
+  links_on_pages.each_with_index do |links, item_index|
+    
     puts "Searching on page №#{item_index + 1}"
 
     links.each do |link|
       doc = Nokogiri::HTML(Curl.get(link).body_str)
-
+      
       name = doc.search('h1.product_main_name').xpath('text()')
       prices = doc.search('span.price_comb').map { |item| item.xpath('text()') }
       image = doc.search('img#bigpic').map { |item| item['src'] }
-
+      
       doc.search('span.radio_label').map { |item| item.xpath('text()') }.each_with_index do |item, price_item|
         csv << ["#{name} #{item}", prices[price_item], image[0]]
       end
