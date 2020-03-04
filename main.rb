eea7ce8986f18efcb4f16ef58b868003770d408b
n.shmelyov <shmelev507@gmail.com>
2020-03-04 03:12:44 +0300

    work with styles

--- main.rb
+++ main.rb
@@ -1,67 +1,58 @@
-# require 'csv'
+require 'csv'
 require 'nokogiri'
-# require 'curb'
+require 'curb'
 require 'open-uri'
-
-
-
+require "css_parser"
 # puts('print link')
 # url = gets.chomp
 # puts('print file name')
 # csv_name = gets.chomp
-url = 'https://www.petsonic.com/snacks-higiene-dental-para-perros/'
-doc = Nokogiri::HTML(open(url))
-links = doc.search('div.pro_outer_box a.product-name').map { |link| link['href'] }
-# puts(links)
+
+http = Curl.get("https://www.petsonic.com/pienso-veterinario-royal-canin-veterinary-diets-productos-veterinarios-para-perros/?p=2")
+doc = Nokogiri::HTML(http.body_str)
+
+button = doc.search("div.af button[@class='loadMore next button lnk_view btn btn-default']")
+css = CssParser::Parser.new
+puts http.body_str.class
+
+
+css.add_block!(http.body_str)
+puts button.to_s.class
+# links = doc.search('div.pro_outer_box a.product-name').map { |link| link['href'] }
 
 # links.each do |link|
+#   http = Curl.get(link)
+#   doc = Nokogiri::HTML(http.body_str)
+#   # doc = Nokogiri::HTML(open(link))
 
-#     html = Nokogiri::HTML(open(link))
+#   labels = doc.search('label.attribute_label').map { |item| item.xpath('text()') }
 
-# sad.search('label.attribute_label').xpath('text()')
-
-
-
-
-
-
-
-
-
-
+#   name = doc.search('h1.product_main_name').xpath('text()')
+#   prices = doc.search('li.no_disponible span.price_comb').map { |item| item.xpath('text()') }
+#   image = doc.search('img#bigpic').map { |item| item['src'] }
+#   doc.search('span.radio_label').map { |item| item.xpath('text()') }.each_with_index do |item, index|
+#     puts "#{name} #{item} #{prices[index]} #{image[0]}"
+#   end
 # end
 
 
-doc = Nokogiri::HTML(open('https://www.petsonic.com/greenie-pack-original-petite-para-perro.html'))
 
+html_string = http.body_str
+html = Nokogiri::HTML(html_string)
+css = CssParser::Parser.new
+css.add_block!(html_string) # Warning:  This line modifies the string passed into it.  In potentially bad ways.  Make sure the string has been duped and stored elsewhere before passing this.
 
-
-
-labels = doc.search('label.attribute_label').map { |item| item.xpath('text()').to_s }.map { |item| item[0, item.length - 1]}
-
-name = doc.search('h1.product_main_name').xpath('text()')
-prices = doc.search('li.no_disponible span.price_comb').map { |item| item.xpath('text()').to_s }
-image = doc.search('img#bigpic').map { |item| item['src'] }
-puts image
-if labels.include?('Peso')
-  doc.search('span.radio_label').map { |item| item.xpath('text()').to_s }.each_with_index do |item, index|
-    puts "#{name} #{item} #{prices[index]} #{image[0]}"
+css.each_selector do |selector, declarations, specificity|
+  next unless selector =~ /^[\d\w\s\#\.\-]*$/ # Some of the selectors given by css_parser aren't actually selectors.
+  begin
+    elements = html.css(selector)
+    elements.each do |match|
+      match["style"] = [match["style"], declarations].compact.join(" ")
+    end
+  rescue
+    
   end
 end
 
-
-
-# labels.each do |i|
-#   puts i.to_s[0, i.to_s.length-1]
-# end
-# puts doc.search('li.no_disponible span.radio_label').map { |link| link.xpath('text()') }[0].class
-# if labels.map{ |item| item.to_s[0, item.to_s.length-1] }.include?('Peso')
-#   if doc.search('span.radio_label').xpath('text()')
-# end
-# puts labels.map{ |item| item.to_s }
-# puts labels[0].to_s[0, labels[0].to_s.length-1]
-# a.map{ |item| item.to_s }.each do |i|
-#     puts i[0,i.length-1], i[0,i.length-1]=='Peso'
-# end
-
-# puts a.map{ |item| item.to_s[0, 4] }
+html_with_inline_styles = html.to_s
+puts html_with_inline_styles
